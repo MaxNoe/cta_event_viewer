8da65bd7b55d6b93c72f082baf87a8a6979ffa34
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-08-14 11:11:49 +0200

    rotate LST view

--- telescope/__init__.py
+++ telescope/__init__.py
@@ -1,6 +1,13 @@
 import pandas as pd
+from numpy import cos, sin, deg2rad
 
 lst_mapping = pd.read_csv('telescope/lst.csv')
+
+angle = deg2rad(-19.5)
+x = cos(angle) * lst_mapping.pixel_x - sin(angle) * lst_mapping.pixel_y
+y = cos(angle) * lst_mapping.pixel_y + sin(angle) * lst_mapping.pixel_x
+lst_mapping.pixel_x = x
+lst_mapping.pixel_y = y
 
 
 class Telescope(object):
@@ -24,7 +31,7 @@ class LST(Telescope):
     n_pixel = len(pixel_x)
     pixel_shape = 'hexagon'
     pixel_size = 0.025
-    pixel_orientation = 0
+    pixel_orientation = angle
 
     def __init__(self, position_x, position_y, telescope_id):
         super().__init__(position_x, position_y, telescope_id)
