279c140be429adbc531e88955122ad749c3f6873
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-08-15 17:00:21 +0200

    get grid of unnecessary white space

--- windows/__init__.py
+++ windows/__init__.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use('tkagg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from matplotlib.axes import Axes
 
 from plots import CameraPlot
 
@@ -14,13 +15,15 @@ class TelescopeEventView(tk.Frame, object):
         self.telescope = telescope
         super(TelescopeEventView, self).__init__(root)
         self.figure = Figure(figsize=(5, 5), facecolor='none')
-        self.ax = self.figure.add_subplot(1, 1, 1, aspect=1)
+        self.ax = Axes(self.figure, [0, 0, 1, 1], aspect=1)
+        self.ax.set_axis_off()
+        self.figure.add_axes(self.ax)
         self.camera_plot = CameraPlot(telescope, self.ax, data, *args, **kwargs)
-        self.figure.set_tight_layout(True)
         self.canvas = FigureCanvasTkAgg(self.figure, master=self)
         self.canvas.show()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.canvas._tkcanvas.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.canvas._tkcanvas.config(highlightthickness=0)
 
     @property
     def data(self):
